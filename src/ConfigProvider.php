971f5c291c05b318980f802d6bfc310e3e76ddbd
D0ng1 <48009708+D0ng1@users.noreply.github.com>
2025-04-09 15:48:24 +0800

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -13,36 +13,34 @@ class ConfigProvider
 
     public function __invoke(): array
     {
+        $this->publishMigrations();
+    
         return [
-            'commands' => [
-                CacheReset::class
-            ],
-            'publish' => [
-                [
-                    'id' => 'config',
-                    'description' => 'The config for permission.',
-                    'source' => __DIR__ . '/../publish/permission.php',
-                    'destination' => BASE_PATH . '/config/autoload/permission.php',
+            'dependencies' => [],
+            'commands' => [],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
                 ],
-                [
-                    'id' => 'database',
-                    'description' => 'The database for permission.',
-                    'source' => __DIR__ . '/../database/migrations/create_permission_tables.php.stub',
-                    'destination' => $this->getMigrationFileName(),
-                ]
             ],
         ];
     }
 
-    protected function getMigrationFileName(): string
+    private function publishMigrations()
     {
-        $timestamp = date('Y_m_d_His');
-        $filesystem = new Filesystem();
-        return Collection::make(BASE_PATH . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
-                        ->flatMap(function ($path) use ($filesystem) {
-                            return $filesystem->glob($path . '*_create_permission_tables.php');
-                        })->push(BASE_PATH . "/migrations/{$timestamp}_create_permission_tables.php")
-                        ->first();
+        $sourcePath = __DIR__ . '/../database/migrations';
+        $targetPath = BASE_PATH . '/migrations';
+    
+        if (! is_dir($targetPath)) {
+            mkdir($targetPath, 0755, true);
+        }
+    
+        foreach (glob($sourcePath . '/*.php') as $file) {
+            $filename = date('Y_m_d_His') . '_' . basename($file);
+            copy($file, $targetPath . '/' . $filename);
+        }
     }
 
 }
